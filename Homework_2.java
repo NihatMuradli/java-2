a42e1eac3fc7a0da9e27d8c9e5634d89ccf80624
NihatMuradli <106442654+NihatMuradli@users.noreply.github.com>
2022-11-06 10:20:36 +0400

    Homework2

---
+++ Homework_2.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Homework_2 {
+	public static void main(String[] args) {
+		int bookPageCount = 250;
+		if(bookPageCount>300) {
+			System.out.println("Bu kitab qalındır");
+		}else {
+			System.out.println("Bu kitab qalın deyil");
+		}
+		
+		System.out.println(5%2);
+		System.out.println(10%4);
+		System.out.println(34%7);
+		
+		int a= 3;
+		System.out.println(a+++(++a)+a++);
+    }
+}
